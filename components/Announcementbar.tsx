5fcea1566e3eef0cae08d22d322d104a9f949c71
peprahobedadjei <nanakwameadjeipeprah@gmail.com>
2026-05-21 01:18:32 +0100

    added announcement bar

---
+++ components/Announcementbar.tsx
@@ -0,0 +1,15 @@
+export default function AnnouncementBar() {
+  return (
+    <>
+      <div className="text-xs bg-[#53093a] px-4 py-2 text-white">
+        <p className="text-center font-medium">
+          BSC Mentorship 2026 Applications Now Open. {''}
+          <a href="#" className="inline-block underline">
+            {" "}
+            Apply Now{" "}
+          </a>
+        </p>
+      </div>
+    </>
+  );
+}
